4b1f07fd50ae36f581a83488cb05a999ba9b8eda
Aymeric Beaumet <hi@aymericbeaumet.com>
2021-02-17 23:41:56 +0100

    simple mappings

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/aymericbeaumet/prox
 
 go 1.15
+
+require (
+	github.com/mattn/go-shellwords v1.0.11
+	gopkg.in/yaml.v2 v2.4.0
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
+)
--- main.go
+++ main.go
@@ -5,7 +5,29 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+
+	"reflect"
+
+	"github.com/mattn/go-shellwords"
+	"gopkg.in/yaml.v3"
 )
+
+// TODO: load from config file
+var configStr = `
+mappings:
+
+  git :
+    hub status -sb
+
+  git st :
+    hub status
+
+  git co ... :
+    hub checkout ...
+
+  git ... :
+    hub ...
+`
 
 func main() {
 	if len(os.Args) < 2 {
@@ -13,15 +35,15 @@ func main() {
 		return
 	}
 
-	name := os.Args[1]
-	var args []string
-	if len(os.Args) >= 3 {
-		args = os.Args[2:]
+	engine, err := NewEngineFromString(configStr)
+	if err != nil {
+		panic(err)
 	}
 
-	name, args = remap(name, args)
-
-	cmd := exec.CommandContext(context.Background(), name, args...)
+	env, args := engine.Map(os.Environ(), os.Args[1:])
+	fmt.Println(env, args)
+	cmd := exec.CommandContext(context.Background(), args[0], args[1:]...)
+	cmd.Env = env
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -37,9 +59,61 @@ func main() {
 	os.Exit(state.ExitCode())
 }
 
-func remap(name string, args []string) (string, []string) {
-	if len(args) == 0 {
-		return "hub", []string{"status", "-sb"}
+type Config struct {
+	Mappings map[string]string
+}
+
+type Engine struct {
+	mappings []*Mapping
+}
+
+type Mapping struct {
+	from []string
+	to   []string
+	env  []string
+}
+
+func NewEngineFromString(configString string) (*Engine, error) {
+	engine := &Engine{}
+
+	var config Config
+	if err := yaml.Unmarshal([]byte(configString), &config); err != nil {
+		return nil, err
 	}
-	return "hub", args
+
+	for fromString, toString := range config.Mappings {
+		from, err := shellwords.Parse(fromString)
+		if err != nil {
+			panic(err)
+		}
+		env, to, err := shellwords.ParseWithEnvs(toString)
+		if err != nil {
+			panic(err)
+		}
+		engine.mappings = append(engine.mappings, &Mapping{
+			from: from,
+			to:   to,
+			env:  env,
+		})
+	}
+
+	return engine, nil
+}
+
+func (e *Engine) Map(env []string, args []string) ([]string, []string) {
+	for _, mapping := range e.mappings {
+		if mapping.from[len(mapping.from)-1] == "..." {
+			from := mapping.from[:len(mapping.from)-1]
+			if reflect.DeepEqual(args[:len(from)], from) {
+				to := append(mapping.to[:len(mapping.to)-1], args[len(from):]...)
+				return append(env, mapping.env...), to
+			}
+		}
+
+		if reflect.DeepEqual(args, mapping.from) {
+			return append(env, mapping.env...), mapping.to
+		}
+	}
+
+	return env, args
 }
