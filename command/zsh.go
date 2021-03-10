704b49e7a10742f2979da7a56a1f9edc561d947d
Aymeric Beaumet <hi@aymericbeaumet.com>
2021-03-10 13:59:43 +0100

    feat: add mutually exclusive flags to zsh completion

--- command/zsh.go
+++ command/zsh.go
@@ -2,7 +2,6 @@ package command
 
 import (
 	"fmt"
-	"os"
 	"strings"
 
 	"github.com/urfave/cli/v2"
@@ -13,35 +12,37 @@ var zshCommand = &cli.Command{
 	Usage:           "Print the Zsh config and exit (aliases and completion)",
 	SkipFlagParsing: true,
 	Action: func(c *cli.Context) error {
-		if err := c.App.Command("--shell").Run(c); err != nil {
+		eng, err := initializeEngine(c)
+		if err != nil {
 			return err
 		}
-		_, err := fmt.Fprintln(c.App.Writer, `
-      _pimp() {
-        eval "$(pimp --zsh-completion -- "${words[@]}")"
-      }
 
-      compdef _pimp pimp
+		for _, executable := range eng.Executables() {
+			fmt.Fprintf(c.App.Writer, "alias %#v=%#v\n", executable, "pimp "+executable)
+		}
+
+		fmt.Fprintln(c.App.Writer, `
+_pimp() {
+  eval "$(pimp --zsh-completion -- "${words[@]}")"
+}
+
+compdef _pimp pimp
     `)
-		return err
+
+		return nil
 	},
 }
 
-// Amazing resource: https://blog.kloetzl.info/how-to-write-a-zsh-completion/
+// Writing Zsh completion functions it not an easy task, here are some
+// resources that were very helpful:
+// - http://zsh.sourceforge.net/Doc/Release/Completion-System.html#Completion-System
+// - https://blog.kloetzl.info/how-to-write-a-zsh-completion/
 var zshCompletionCommand = &cli.Command{
 	Name:            "--zsh-completion",
 	Hidden:          true,
 	SkipFlagParsing: true,
 	Action: func(c *cli.Context) error {
-		var args []string
-		for i, arg := range os.Args {
-			if arg == "--" {
-				args = os.Args[i+1:]
-				break
-			}
-		}
-
-		lastArg := args[len(args)-1]
+		sharedExclusionList := []string{"-h", "--help", "-v", "--version"}
 
 		fmt.Fprintln(c.App.Writer, `
 local ret=1
@@ -50,41 +51,74 @@ local -a options
 options+=(
     `)
 
+		fmt.Fprintln(c.App.Writer, `+ '(commands)'`) //
 		for _, command := range c.App.VisibleCommands() {
-			if strings.HasPrefix(command.Name, lastArg) {
-				s := command.Name + "[" + command.Usage + "]"
-				fmt.Fprintf(c.App.Writer, "'%s'\n", s)
-			}
+			fmt.Fprintln(c.App.Writer, `'(- *)`+command.Name+"["+command.Usage+`]'`)
 		}
 
+		fmt.Fprintln(c.App.Writer, "")
+
+		fmt.Fprintln(c.App.Writer, `+ 'flags'`) //
 		for _, flag := range c.App.VisibleFlags() {
+			exclusionList := append([]string{}, sharedExclusionList...)
+			flagList := []string{}
+
 			for _, name := range flag.Names() {
-				var prefixedFlag string
-				var suffix string
+				var flagName, suffix string
 				if len(name) == 1 {
-					prefixedFlag = "-" + name
-					suffix = "+"
+					flagName = "-" + name
 				} else {
-					prefixedFlag = "--" + name
-					suffix = "="
-				}
-				if strings.HasPrefix(prefixedFlag, lastArg) {
-					s := prefixedFlag + suffix + "[" + getFlagUsage(flag) + "]"
-					if isFlagTakesFile(flag) {
-						s += ":file:_files:"
+					flagName = "--" + name
+					if _, ok := flag.(*cli.StringFlag); ok {
+						suffix = "="
 					}
-					fmt.Fprintf(c.App.Writer, "'%s'\n", s)
 				}
+
+				if contains(sharedExclusionList, flagName) {
+					exclusionList = []string{"-", "*"}
+				} else {
+					exclusionList = append(exclusionList, flagName)
+				}
+
+				flagList = append(flagList, flagName+suffix)
 			}
+
+			fmt.Fprint(c.App.Writer, `'`)
+
+			if len(exclusionList) > 1 {
+				fmt.Fprint(c.App.Writer, `(`+strings.Join(exclusionList, " ")+`)`)
+			}
+
+			if len(flagList) > 1 {
+				fmt.Fprint(c.App.Writer, `'{`+strings.Join(flagList, ",")+`}'`)
+			} else {
+				fmt.Fprint(c.App.Writer, flagList[0])
+			}
+
+			fmt.Fprint(c.App.Writer, "["+getFlagUsage(flag)+"]")
+
+			if isFlagTakesFile(flag) {
+				fmt.Fprint(c.App.Writer, ":file:_files")
+			}
+
+			fmt.Fprintln(c.App.Writer, `'`)
 		}
 
 		fmt.Fprintln(c.App.Writer, `
 )
 
-_arguments -w -s -S $options[@] && ret=0
-return ret
-    `)
+_arguments -S "$options[@]" && ret=0
+return ret`)
 
 		return nil
 	},
+}
+
+func contains(stack []string, needle string) bool {
+	for _, s := range stack {
+		if s == needle {
+			return true
+		}
+	}
+	return false
 }
