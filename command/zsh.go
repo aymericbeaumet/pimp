b428d6f4a6c19c6f5617010c530209c77a8c927b
Aymeric Beaumet <hi@aymericbeaumet.com>
2021-03-11 19:33:51 +0100

    feat: improve zsh completion

--- command/zsh.go
+++ command/zsh.go
@@ -41,18 +41,40 @@ var zshCompletionCommand = &cli.Command{
 	Name:   "--zsh-completion",
 	Hidden: true,
 	Action: func(c *cli.Context) error {
-		args := c.Args().Slice()
-
-		if len(args) >= 2 && args[0] == "pimp" && !strings.HasPrefix(args[1], "-") {
-			// TODO: parse args + expannd BIN ARGS to provide accurate completion
-			if len(args) == 2 {
-				fmt.Fprintln(c.App.Writer, "_path_commands")
-			} else {
-				delta := 1
-				fmt.Fprintf(c.App.Writer, "shift %d words\n", delta)
-				fmt.Fprintf(c.App.Writer, "(( CURRENT -= %d ))\n", delta)
-				fmt.Fprintf(c.App.Writer, "_normal -p %#v\n", args[1])
+		if args := c.Args().Slice(); len(args) >= 2 { // pimp CMD [ARG]...
+			eng, err := initializeEngine(c, true, true)
+			if err != nil {
+				return err
 			}
+
+			// todo: extract args of this pimp command via the flags parser
+
+			if len(args) == 2 {
+				if cmds := eng.Commands(); len(cmds) > 0 {
+					fmt.Fprintln(c.App.Writer, "local -a pcmds; pcmds=(")
+					for _, cmd := range cmds {
+						fmt.Fprintf(c.App.Writer, "%#v\n", cmd)
+					}
+					fmt.Fprintln(c.App.Writer, ")")
+					fmt.Fprintln(c.App.Writer, "_describe 'pimp command' pcmds")
+				}
+				fmt.Fprintln(c.App.Writer, "_path_commands")
+				return nil
+			}
+
+			_, expandedArgs, _ := eng.Map(nil, args[1:])
+			if len(expandedArgs[len(expandedArgs)-1]) != 0 && len(args[len(args)-1]) == 0 {
+				expandedArgs = append(expandedArgs, "")
+			}
+			current := len(expandedArgs) // index starts at 1
+			fmt.Fprintln(c.App.Writer, "words=(")
+			for _, word := range expandedArgs {
+				fmt.Fprintf(c.App.Writer, "%#v\n", word)
+			}
+			fmt.Fprintln(c.App.Writer, ")")
+			fmt.Fprintf(c.App.Writer, "(( CURRENT = %d ))\n", current)
+			fmt.Fprintf(c.App.Writer, "_normal -p %#v\n", expandedArgs[0])
+
 			return nil
 		}
 
