42e329d3c0ae827d1bebd68120fcbf2bcc319158
Aymeric Beaumet <hi@aymericbeaumet.com>
2021-03-16 22:29:53 +0100

    feat: improve the REPL with history, completion, ...

--- command/repl.go
+++ command/repl.go
@@ -1,53 +1,86 @@
 package command
 
 import (
-	"bufio"
 	"fmt"
+	"reflect"
+	"regexp"
 	"strings"
 
+	prompt "github.com/c-bata/go-prompt"
+	"github.com/sahilm/fuzzy"
 	"github.com/urfave/cli/v2"
 )
+
+var identifierRegexp = regexp.MustCompilePOSIX("^[a-zA-Z][a-zA-Z0-9]*$")
 
 var replCommand = &cli.Command{
 	Hidden: true,
 	Action: func(c *cli.Context) error {
-		const prompt = "pimp> "
 		var sb strings.Builder
 
-		fmt.Fprint(c.App.Writer, prompt)
-
-		scanner := bufio.NewScanner(c.App.Reader)
-		for scanner.Scan() {
-			text := strings.TrimSpace(scanner.Text())
-
-			if len(text) == 0 {
-				fmt.Fprint(c.App.Writer, prompt)
-				continue
+		executor := func(input string) {
+			input = strings.TrimSpace(input)
+			if len(input) == 0 {
+				return
 			}
 
 			sb.Reset()
 			sb.WriteString(c.String("ldelim"))
 			sb.WriteRune(' ')
-			sb.WriteString(scanner.Text())
+			sb.WriteString(strings.TrimSpace(input))
 			sb.WriteRune(' ')
 			sb.WriteString(c.String("rdelim"))
 
 			rendered, err := render(sb.String(), c.String("ldelim"), c.String("rdelim"))
 			if err != nil {
 				fmt.Fprintln(c.App.ErrWriter, err)
-				fmt.Fprint(c.App.Writer, prompt)
-				continue
+			} else {
+				fmt.Fprint(c.App.Writer, rendered)
+				if !strings.HasSuffix(rendered, "\n") {
+					fmt.Fprint(c.App.Writer, "\n")
+				}
 			}
-
-			fmt.Fprint(c.App.Writer, rendered)
-
-			if !strings.HasSuffix(rendered, "\n") {
-				fmt.Fprint(c.App.Writer, "\n")
-			}
-
-			fmt.Fprint(c.App.Writer, prompt)
 		}
 
-		return scanner.Err()
+		completer := func(document prompt.Document) []prompt.Suggest {
+			if strings.HasSuffix(document.Text, " ") {
+				return nil
+			}
+
+			args := strings.Fields(document.Text)
+			if len(args) == 0 {
+				return nil
+			}
+			lastArg := args[len(args)-1]
+			if !identifierRegexp.MatchString(lastArg) {
+				return nil
+			}
+
+			pattern := lastArg
+			data := make([]string, 0, len(fm))
+			for fn := range fm {
+				data = append(data, fn)
+			}
+			matches := fuzzy.Find(pattern, data)
+
+			out := make([]prompt.Suggest, 0, len(matches))
+			for _, match := range matches {
+				out = append(out, prompt.Suggest{
+					Text:        match.Str,
+					Description: reflect.TypeOf(fm[match.Str]).String(),
+				})
+			}
+			return out
+		}
+
+		p := prompt.New(
+			executor,
+			completer,
+			prompt.OptionTitle("pimp"),
+			prompt.OptionPrefix("pimp> "),
+		)
+		p.Run()
+
+		return nil
 	},
 }
--- go.mod
+++ go.mod
@@ -7,15 +7,19 @@ require (
 	github.com/Masterminds/semver v1.5.0 // indirect
 	github.com/Masterminds/sprig v2.22.0+incompatible
 	github.com/blang/semver/v4 v4.0.0
+	github.com/c-bata/go-prompt v0.2.6
 	github.com/go-git/go-git/v5 v5.2.0
 	github.com/google/uuid v1.2.0 // indirect
 	github.com/huandu/xstrings v1.3.2 // indirect
+	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/mattn/go-shellwords v1.0.11
 	github.com/mitchellh/copystructure v1.1.1 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/pelletier/go-toml v1.8.1
+	github.com/sahilm/fuzzy v0.1.0
 	github.com/urfave/cli/v2 v2.3.0
 	gopkg.in/yaml.v2 v2.4.0
+	k8s.io/api v0.20.4
 	k8s.io/apimachinery v0.20.4
 	k8s.io/client-go v0.20.4
 )
