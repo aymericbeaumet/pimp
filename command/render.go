f59836c6ac32c2674b206ffd6e841df2f33facd2
Aymeric Beaumet <hi@aymericbeaumet.com>
2021-03-16 13:26:43 +0100

    feat: make --render accept a single file only

--- command/render.go
+++ command/render.go
@@ -1,6 +1,7 @@
 package command
 
 import (
+	"fmt"
 	"io"
 	"os"
 	"strings"
@@ -11,54 +12,52 @@ import (
 
 var renderCommand = &cli.Command{
 	Name:      "--render",
-	ArgsUsage: "[FILE]...",
-	Usage:     "Sequentially open and render the template FILES (- for stdin)",
+	ArgsUsage: "FILE",
+	Usage:     "Render the template FILE (- for stdin)",
 	Action: func(c *cli.Context) error {
-		var readerCache []byte
+		args := c.Args().Slice()
+		if len(args) != 1 {
+			return fmt.Errorf("--render expects exactly one FILE, got %d", len(args))
+		}
 
-		for _, arg := range c.Args().Slice() {
-			var text string
+		var text string
 
-			if arg == "-" {
-				if readerCache == nil {
-					bytes, err := io.ReadAll(c.App.Reader)
-					if err != nil {
-						return err
-					}
-					readerCache = bytes
-				}
-				text = string(readerCache)
-			} else {
-				renderFilepath, err := normalize.Path(arg)
-				if err != nil {
-					return err
-				}
-
-				data, err := os.ReadFile(renderFilepath)
-				if err != nil {
-					return err
-				}
-
-				text = string(data)
+		if args[0] == "-" {
+			bytes, err := io.ReadAll(c.App.Reader)
+			if err != nil {
+				return err
 			}
-
-			// strip shebang if found
-			if strings.HasPrefix(text, "#!") {
-				if newlineIndex := strings.IndexRune(text, '\n'); newlineIndex > -1 {
-					text = text[newlineIndex+1:]
-				} else {
-					text = ""
-				}
-			}
-
-			rendered, err := render(text, c.String("ldelim"), c.String("rdelim"))
+			text = string(bytes)
+		} else {
+			renderFilepath, err := normalize.Path(args[0])
 			if err != nil {
 				return err
 			}
 
-			if _, err := c.App.Writer.Write([]byte(rendered)); err != nil {
+			data, err := os.ReadFile(renderFilepath)
+			if err != nil {
 				return err
 			}
+
+			text = string(data)
+		}
+
+		// strip shebang if found
+		if strings.HasPrefix(text, "#!") {
+			if newlineIndex := strings.IndexRune(text, '\n'); newlineIndex > -1 {
+				text = text[newlineIndex+1:]
+			} else {
+				text = ""
+			}
+		}
+
+		rendered, err := render(text, c.String("ldelim"), c.String("rdelim"))
+		if err != nil {
+			return err
+		}
+
+		if _, err := c.App.Writer.Write([]byte(rendered)); err != nil {
+			return err
 		}
 
 		return nil
