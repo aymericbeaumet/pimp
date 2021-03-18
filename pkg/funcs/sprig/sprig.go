c69011d21b5e4f56842e5f58fb8a585bb58b3c89
Aymeric Beaumet <hi@aymericbeaumet.com>
2021-03-18 11:23:27 +0100

    refactor: no need to support the sprig "must" functions

--- pkg/funcs/sprig/sprig.go
+++ pkg/funcs/sprig/sprig.go
@@ -2,7 +2,6 @@
 package sprig
 
 import (
-	"strings"
 	"text/template"
 
 	"github.com/Masterminds/sprig"
@@ -16,27 +15,13 @@ var sprigIgnore = map[string]struct{}{
 	"toString":     {},
 }
 
-const sprigMustPrefix = "must"
-
 func FuncMap() template.FuncMap {
 	in := sprig.TxtFuncMap()
 	out := template.FuncMap{}
 
 	for name, fn := range in {
-		// skip all the functions from the ignore list
 		if _, ok := sprigIgnore[name]; ok {
 			continue
-		}
-
-		// skip all the "must" functions
-		if strings.HasPrefix(name, sprigMustPrefix) {
-			continue
-		}
-
-		// try to find a must variant and use it if found
-		mustVariant := sprigMustPrefix + strings.ToUpper(name[0:1]) + name[1:]
-		if _, ok := in[mustVariant]; ok {
-			name = mustVariant
 		}
 
 		out[name] = fn
