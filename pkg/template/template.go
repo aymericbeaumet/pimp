a3e1a2c86078ffb7c224041669bc36bf2ede039d
Aymeric Beaumet <hi@aymericbeaumet.com>
2021-03-19 01:06:30 +0100

    refactor: remove template.RenderString

--- pkg/template/template.go
+++ pkg/template/template.go
@@ -57,24 +57,18 @@ func Render(w io.Writer, text, ldelim, rdelim string, fm template.FuncMap) error
 	return err
 }
 
-func RenderString(text, ldelim, rdelim string, fm template.FuncMap) (string, error) {
-	var sb strings.Builder
-	err := Render(&sb, text, ldelim, rdelim, fm)
-	return sb.String(), err
-}
-
 // RenderStrings renders several strings in a single context. This makes it
 // possible to interact between several templates with variable declarations,
 // etc. This could generate empty strings in the output that have to be dealt
 // with.
 func RenderStrings(texts []string, ldelim, rdelim string, fm template.FuncMap) ([]string, error) {
 	const SEP = "\x00pimp\x00"
-
 	text := strings.Join(texts, SEP)
-	rendered, err := RenderString(text, ldelim, rdelim, fm)
-	if err != nil {
+
+	var sb strings.Builder
+	if err := Render(&sb, text, ldelim, rdelim, fm); err != nil {
 		return nil, err
 	}
 
-	return strings.Split(rendered, SEP), nil
+	return strings.Split(sb.String(), SEP), nil
 }
