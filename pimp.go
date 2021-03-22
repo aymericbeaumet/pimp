9a85f364207fe9cd1ae6a5fc9d2a8b880c4bfc3a
Aymeric Beaumet <hi@aymericbeaumet.com>
2021-03-22 14:01:18 +0100

    feat: github.com/aymeric/pimp can now be imported

---
+++ pimp.go
@@ -0,0 +1,13 @@
+package pimp
+
+import (
+	"github.com/aymericbeaumet/pimp/pkg/funcs"
+	"github.com/aymericbeaumet/pimp/pkg/script"
+	"github.com/aymericbeaumet/pimp/pkg/template"
+)
+
+var FuncMap = funcs.FuncMap
+
+var Render = template.Render
+
+var Run = script.Run
